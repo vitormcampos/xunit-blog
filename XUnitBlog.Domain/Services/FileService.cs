0664290e65a7d99421464a32750e3d9cce855beb
Vitor Campos <vitormcampos99@gmail.com>
2025-11-23 15:35:55 -0300

    feat(files): Adiciona service para upload de arquivos - Adiciona metodos para adicionar e remover - Adiciona teste para upload - Adiciona teste para remoção

---
+++ XUnitBlog.Domain/Services/FileService.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace XUnitBlog.Domain.Services;
+
+public class FileService(string wwwrootPath)
+{
+    private readonly string _uploadsPath = Path.Combine(wwwrootPath, "uploads");
+
+    public async Task<string> UploadAsync(IFormFile file)
+    {
+        var fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+        var filePath = Path.Combine(_uploadsPath, fileName);
+
+        if (!Directory.Exists(_uploadsPath))
+        {
+            Directory.CreateDirectory(_uploadsPath);
+        }
+
+        using (var stream = new FileStream(filePath, FileMode.Create))
+        {
+            await file.CopyToAsync(stream);
+        }
+
+        if (!File.Exists(filePath))
+        {
+            throw new Exception("Erro ao salvar o arquivo");
+        }
+
+        return Path.Combine("uploads", fileName);
+    }
+
+    public bool Delete(string filePath)
+    {
+        try
+        {
+            var fileName = Path.GetFileName(filePath);
+            var fullFilePath = Path.Combine(_uploadsPath, fileName);
+
+            if (File.Exists(fullFilePath))
+            {
+                File.Delete(fullFilePath);
+            }
+
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+}
---
+++ XUnitBlog.Test/Files/FileServiceTest.cs
@@ -0,0 +1,69 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Http;
+using XUnitBlog.Domain.Services;
+
+namespace XUnitBlog.Test.Files;
+
+public class FileServiceTest : IDisposable
+{
+    private readonly string _testUploadPath;
+    private readonly FileService _fileService;
+
+    public FileServiceTest()
+    {
+        _testUploadPath = Path.Combine(Directory.GetCurrentDirectory(), "temp");
+        _fileService = new FileService(_testUploadPath);
+    }
+
+    [Fact]
+    public async Task ShouldSaveFileToFileSystem()
+    {
+        // Arrange
+        var bytes = Encoding.UTF8.GetBytes("Um arquivo de teste");
+        var file = new FormFile(new MemoryStream(bytes), 0, bytes.Length, "Data", "teste.txt");
+
+        // Action
+        var filePath = await _fileService.UploadAsync(file);
+
+        // Assert
+        Assert.NotEmpty(filePath);
+    }
+
+    [Fact]
+    public async Task ShouldSaveFileWhenUploadPathDoesNotExist()
+    {
+        // Arrange
+        var bytes = Encoding.UTF8.GetBytes("Um arquivo de teste");
+        var file = new FormFile(new MemoryStream(bytes), 0, bytes.Length, "Data", "teste.txt");
+
+        // Action
+        if (Directory.Exists(_testUploadPath))
+        {
+            Directory.Delete(_testUploadPath, true);
+        }
+
+        var filePath = await _fileService.UploadAsync(file);
+
+        // Assert
+        Assert.NotEmpty(filePath);
+    }
+
+    [Fact]
+    public async Task ShouldDeleteFile()
+    {
+        // Arrange
+        var bytes = Encoding.UTF8.GetBytes("Um arquivo de teste");
+        var file = new FormFile(new MemoryStream(bytes), 0, bytes.Length, "Data", "teste.txt");
+
+        // Action
+        var filePath = await _fileService.UploadAsync(file);
+        var result = _fileService.Delete(filePath);
+
+        Assert.True(result);
+    }
+
+    public void Dispose()
+    {
+        Directory.Delete(_testUploadPath, true);
+    }
+}
